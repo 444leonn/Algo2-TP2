9868af513db857cc463025ee413f0ae498bd3c85
444leonn <acostaleon25@gmail.com>
2025-11-13 16:07:27 -0300

    add: constantes

---
+++ src/constantes.h
@@ -0,0 +1,37 @@
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+#define TROJO "\x1b[31m"
+#define TNORMAL "\x1b[0m"
+
+#define TIPO_INVALIDO -1
+#define POSICION_INVALIDA -1
+
+#define MODO_LECTURA "r"
+#define MODO_ESCRITURA "w"
+
+#define TAMAÑO_INICIAL 15
+#define CAPACIDAD_INICIAL 4
+
+#define CANTIDAD_EJEMPLO 10
+#define DATO_EJEMPLO_1 5
+#define DATO_EJEMPLO_2 6
+#define DATO_EJEMPLO_3 7
+#define DATO_EJEMPLO_4 4
+
+#define VALOR_POSITIVO 1
+#define VALOR_NEGATIVO -1
+
+#define OPCION_ID "idIDIdiD"
+#define OPCION_NOMBRE "nombrenombrEnombREnomBREnoMBREnOMBRENOMBRE"
+
+// MENSAJES DE ERROR
+#define CANT_PARAMETROS \
+	"Error, debe ingresar una cantidad de parametros valida."
+#define PARAMETRO_INVALIDO "Error, parametro ingresado invalido."
+#define FALLO_LECTURA_ARCHIVO "Error, ha falla la lectura del archivo."
+#define FALLO_CREACION_ABB "Error, ha fallado creacion de ABB."
+#define NO_ENCONTRADO "El Pokemon no ha sido encontrado."
+#define OPCION_INVALIDA "Error, opcion de busqueda invalida."
+
+#endif // CONSTANTES_H_
