89da49824ecf19498d532b46950d373d89519fb3
444leonn <acostaleon25@gmail.com>
2025-11-21 00:51:54 -0300

    add: aux.c con funciones auxiliares

---
+++ src/aux.c
@@ -0,0 +1,89 @@
+#include "aux.h"
+
+#include "constantes.h"
+#include "ansi.h"
+#include "aux_tp1.h"
+
+void limpiar_buffer()
+{
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF)
+		;
+}
+
+bool evaluar_opciones(char *opcion_evaluada, char *opciones_disponibles)
+{
+	return strstr(opciones_disponibles, opcion_evaluada) != NULL;
+}
+
+void esperar_segundos(size_t cantidad)
+{
+	for (int i = 0; i < cantidad; i++)
+		sleep(1);
+}
+
+bool evaluar_volver_menu(char *opcion_evaluada)
+{
+	if (evaluar_opciones(opcion_evaluada, OPCION_VOLVER) == true) {
+		printf(ANSI_COLOR_BLUE MENSAJE_VOLVIENDO ANSI_COLOR_RESET "\n");
+		esperar_segundos(2);
+		return true;
+	}
+	return false;
+}
+
+char seleccionar_opcion()
+{
+	printf("\n\n" ANSI_COLOR_BOLD MENSAJE_SELECCION ANSI_COLOR_RESET);
+	int aux = fgetc(stdin);
+	int siguiente = fgetc(stdin);
+	if ((char)siguiente != '\n' && (char)siguiente != EOF) {
+		ungetc(siguiente, stdin);
+	}
+	char c = (char)aux;
+	printf("\n");
+
+	return c;
+}
+
+void mostrar_mensaje_cargar()
+{
+	printf(ANSI_COLOR_BLUE MENSAJE_CARGAR_ARCHIVO ANSI_COLOR_RESET "\n\n");
+	printf(ANSI_COLOR_BLUE MENSAJE_INSTRUCCION_ARCHIVO ANSI_COLOR_RESET
+	       "\n");
+	printf(ANSI_COLOR_MAGENTA MENSAJE_VOLVER ANSI_COLOR_RESET "\n\n");
+	esperar_segundos(2);
+}
+
+void mostrar_mensaje_fallo_lectura()
+{
+	printf(ANSI_COLOR_RED FALLO_LECTURA_ARCHIVO ANSI_COLOR_RESET "\n\n");
+	printf(ANSI_COLOR_BLUE MENSAJE_VOLVIENDO ANSI_COLOR_RESET "\n");
+	esperar_segundos(2);
+}
+
+void mostrar_mensaje_opcion_invalida()
+{
+	printf(ANSI_COLOR_RED ANSI_COLOR_BOLD OPCION_INVALIDA ANSI_COLOR_RESET
+	       "\n");
+	printf(ANSI_COLOR_BLUE ANSI_COLOR_BOLD
+		       INTENTE_NUEVAMENTE ANSI_COLOR_RESET "\n\n");
+	sleep(2);
+}
+
+void mostrar_mensaje_continuar()
+{
+	printf("\n" ANSI_COLOR_BLUE MENSAJE_CONTINUAR ANSI_COLOR_RESET);
+	getchar();
+	limpiar_buffer();
+}
+
+char *leer_nombre_archivo()
+{
+	char *nombre = leer_linea(stdin);
+	if (nombre == NULL) {
+		mostrar_mensaje_fallo_lectura();
+		return NULL;
+	}
+	return nombre;
+}
---
+++ src/aux.h
@@ -0,0 +1,60 @@
+#ifndef __AUX_H__
+#define __AUX_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+/*
+*   Limpia el buffer de entrada.
+*/
+void limpiar_buffer();
+
+/*
+*   Evalua dos opciones.
+*/
+bool evaluar_opciones(char *opcion_evaluada, char *opciones_disponibles);
+
+/*
+*   Espera la cantidad de segundos pasada por parametro.
+*/
+void esperar_segundos(size_t cantidad);
+
+/*
+*   Evalua si la opcion es la de volver, y si lo es muestra un mensaje por pantalla.
+*/
+bool evaluar_volver_menu(char *opcion_evaluada);
+
+/*
+*   Muestra un mensaje y pide el ingreso de un caracter.
+*/
+char seleccionar_opcion();
+
+/*
+*   Muestra un mensaje.
+*/
+void mostrar_mensaje_cargar();
+
+/*
+*   Muestra un mensaje
+*/
+void mostrar_mensaje_fallo_lectura();
+
+/*
+*   Muestra un mensaje.
+*/
+void mostrar_mensaje_opcion_invalida();
+
+/*
+*   Muestra un mensaje y espera el ingreso de un caracter.
+*/
+void mostrar_mensaje_continuar();
+
+/*
+*   Lee por stdin el nombre de un archivo.
+*   Si falla muestra un mensaje de error.
+*/
+char *leer_nombre_archivo();
+
+#endif // __AUX_H__
